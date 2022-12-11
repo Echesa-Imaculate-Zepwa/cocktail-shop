351f601413142e424a04ec39d09ebf7e4a263cf5
Echesa-Imaculate-Zepwa <global-imaculate.zepwa@student.moringaschool.com>
2022-12-11 14:03:17 +0300

    edited the cocktail controller

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,58 +1,57 @@
 class CocktailsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
      # GET /cocktails
     def index
-        cocktails = Cocktail.all 
-        render json: cocktails
+      cocktails = Cocktail.all
+      render json: cocktails
+        
     end
 
   # GET /cocktails/:id
     def show
-        cocktail = Cocktail.find_by(id: params[:id])
-        if cocktail
-          render json: cocktail
-        else
-          render json: { error: "cocktail not found" }, status: :not_found
-        end
+      cocktail = find_cocktail
+      render json: cocktail
+      
       end
     
 
 # POST /cocktail
     def create
-        cocktail = Cocktail.create(cocktail_params)
-        if cocktail.valid?
-          render json: cocktail, status: :created
-        else
-          render json: { errors: cocktail.errors }, status: :unprocessable_entity
-    end
+      cocktail = Cocktail.create(bird_params)
+      render json: cocktail, status: :created
   end
 
 
      # PATCH /cocktail/:id
     def update
-        cocktail = Cocktail.find_by(id: params[:id])
-        if cocktail
-          cocktail.update(cocktail_params)
-          render json: cocktail
-        else
-          render json: { error: "cocktail not found" }, status: :not_found
-        end
+      cocktail = find_cocktail
+      cocktail.update(cocktail_params)
+      render json: cocktail
+       
       end
 
 # DELETE /cocktails/:id  
 def destroy
-    cocktail = Cocktail.find_by(id: params[:id])
-    if cocktail
-      cocktail.destroy
-      head :no_content
-    else
-      render json: { error: "cocktail not found" }, status: :not_found
-    end
+  cocktail = find_cocktail
+  cocktail.destroy
+  head :no_content
   end
+
 
   private
 
   def cocktail_params
     params.permit(:name, :category, :image, :price)
+  end
+
+  def find_cocktail
+    Cocktail.find(params[:id])
+  end
+
+
+  def render_not_found_response
+    render json: { error: "Cocktail not found" }, status: :not_found
   end
   
 
--- client/src/App.js
+++ client/src/App.js
@@ -5,57 +5,11 @@ import SearchBar from './components/SearchBar';
 
 function App() {
   const [cocktails, setCocktails] = useState([]);
-  // const [loading, setLoading] = useState([])
-
-
-
-  // useEffect(() => {
-    // const fetchCocktailList = async () => {
-        // const baseUrl = "/cocktails";
-
-        // setLoading(true);
-
-        // try {
-            // const res = await fetch(`${baseUrl}filter.php?c=Cocktail`);
-            // const data = await res.json();
-            // console.log(data);
-
-            // setCocktails(data);
-            // setLoading(false);
-        // } catch (err) {
-            // console.log("Error fetching data");
-
-            // setLoading(false);
-        // }
-      // };
-
-    // fetchCocktailList();
-  // }, []);
-
-
   useEffect(() => {
     fetch("/cocktails")
     .then((response) => response.json())
     .then((data) => setCocktails(data));
   },[])
-  // console.log(cocktails)
-
-
-
-  // const fetchData = () => {
-    // return fetch("http:/localhost:3000/cocktails")
-          // .then((response) => response.json())
-          // .then((data) => setCocktails(data));
-  // }
-  // useEffect(() => {
-    // fetchData();
-  // },[])
-
-
-
- 
-
-
 
   return (
     <>
