61675a8766da8a9462aee9ceebc033d12b5dede4
Echesa-Imaculate-Zepwa <global-imaculate.zepwa@student.moringaschool.com>
2022-12-12 10:58:11 +0300

    setup cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,15 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
 
-  after_filter :cors_set_access_control_headers
+  # after_filter :cors_set_access_control_headers
 
-  def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-    headers['Access-Control-Request-Method'] = '*'
-    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  # def cors_set_access_control_headers
+    # headers['Access-Control-Allow-Origin'] = '*'
+    # headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    # headers['Access-Control-Request-Method'] = '*'
+    # headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
 
-  end
+  # end
+end
   
   
